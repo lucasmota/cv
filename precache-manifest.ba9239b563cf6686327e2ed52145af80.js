9578fa432b34d1012a1a3336c4f2d7700eebae21
Mota <Lucas_Mota@Dell.com>
2020-05-27 21:04:55 -0300

    Updates

---
+++ precache-manifest.ba9239b563cf6686327e2ed52145af80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169a66ae10005be4010801e83109eed3",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "91ba6c605d771109b151",
+    "url": "/cv/static/js/2.a171cd35.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/cv/static/js/2.a171cd35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9777177eeddb7f55604e",
+    "url": "/cv/static/js/main.8a751583.chunk.js"
+  },
+  {
+    "revision": "d6dcef095c971930b9da",
+    "url": "/cv/static/js/runtime-main.e4fe9350.js"
+  },
+  {
+    "revision": "c28d234f4ef30fa9597d5cb353ca0893",
+    "url": "/cv/static/media/myself.c28d234f.png"
+  }
+]);
