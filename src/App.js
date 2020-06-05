094c2ce8f642ae51c06743daaeccc12092517d0c
Mota <Lucas_Mota@Dell.com>
2020-06-05 11:42:47 -0300

    Updated App to arrowFunction and fixed education JSON

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import Box from "./components/box/box.component";
 import CvContent from "./components/cv-content/cv-content.component";
 import { GlobalStyle } from "./global.styles";
 
-function App() {
-  return (
-    <div className="App">
-      <GlobalStyle />
-      <Box styled={true}>
-        <CvContent />
-      </Box>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <GlobalStyle />
+    <Box styled={true}>
+      <CvContent />
+    </Box>
+  </div>
+);
 
 export default App;
